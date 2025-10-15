64e1275b147a52927ec280c84a7a32a9f7dc8146
webbdev <forrwebdev@gmail.com>
2025-10-15 15:12:02 +0100

    update 'back to top' button style

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -81,10 +81,10 @@ const Footer: React.FC = () => {
 					{showButton && (
 						<button
 							onClick={scrollToTop}
-							className="absolute right-[12px] sm:right-[20px] xl:right-[-100px] bottom-3.5 sm:bottom-3 bg-[#efadbd] text-black px-2.5 sm:px-4 xl:px-5 py-2 xl:py-3 rounded-full shadow-lg hover:bg-[#f5c4d1] transition"
+							className="absolute right-[20px] sm:right-[20px] xl:right-[-100px] bottom-3.5 sm:bottom-3 bg-[#efadbd] text-black px-4 sm:px-4 xl:px-5 py-2 xl:py-3 rounded-full shadow-lg hover:bg-[#f5c4d1] transition"
 							aria-label="Back to top"
 						>
-							↑ Top
+							<span>↑</span> <span className="hidden sm:inline">Top</span>
 						</button>
 					)}
 
